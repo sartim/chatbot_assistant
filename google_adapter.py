724f768686fc1b47e8bf0d75732af6610130b181
Tim <sarrtim@gmail.com>
2019-12-27 19:02:17 +0300

    Update google logic adapter to return search result

--- google_adapter.py
+++ google_adapter.py
@@ -2,6 +2,22 @@ import wikipedia
 from chatterbot.conversation import Statement
 from chatterbot.logic import LogicAdapter
 
+from chatbot.helpers.wikipedia_search import WikipediaSearch
+
 
 class GoogleLogicAdapter(LogicAdapter):
-    pass
+    def can_process(self, statement):
+        return True
+
+    def process(
+            self, input_statement,
+            additional_response_selection_parameters=None):
+        import random
+        search_result = WikipediaSearch.get_summary(input_statement)
+        confidence = random.uniform(0, 1)
+        selected_statement = Statement(
+            text=search_result, in_response_to=input_statement,
+            confidence=1
+        )
+        selected_statement.confidence = confidence
+        return selected_statement
